dfbd77dc22e0207216c4bfd32b5f38c5e092de83
tkdrms4585 <76689529+tkdrms4585@users.noreply.github.com>
2021-04-02 00:46:12 +0900

    별찍기

---
+++ src/Asterisk/ast_num8.java
@@ -0,0 +1,26 @@
+package Asterisk;
+
+public class ast_num8 {
+	public static void main(String args[]) {
+		int n = 5;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j != 0 && j != 4 && i == 0)
+					System.out.print("*");
+				else if (j != 0 && j != 4 && i == n - 1)
+					System.out.print("*");
+				else if (j != 0 && j != 4 && i == n/2)
+					System.out.print("*");
+				else if (j == 0 && (i == 1 || i == 3))
+					System.out.print("*");
+				else if (j == n-1 && (i == 1 || i == 3))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
