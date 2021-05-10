19140b669239b08afb1c7c043070eb6f9d0f6c44
tkdrms4585 <76689529+tkdrms4585@users.noreply.github.com>
2021-05-10 12:05:31 +0900

    Backjoon

--- src/BackJoon/B15552.java
+++ src/BackJoon/B15552.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 
-public class Main {
+public class B15552 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
